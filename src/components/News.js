027207836a8811967803ad9b1ea2f1c970f518c1
Aditya Nagre <81866694+AdityaNagre@users.noreply.github.com>
2022-06-18 14:36:06 +0530

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -29,34 +29,34 @@ function News() {
                 <div className="flex  mx-3 my-3 h-96 flex-col   ">
                     <div className="container ">
                     <Slider {...settings}>
-                        <div class=" mb-3 py-2 text-sm text-black bg-blue-100 rounded-lg "  role="alert">
+                        <div class=" mb-3 py-2 text-sm text-black bg-blue-100 rounded-lg cursor-pointer"  role="alert">
                             <div className=' mx-3 pl-2 border-l-4 border-blue-500 md:pb-5 ' >
                                 <h1 className='text-lg font-bold'>NITJ Campus1</h1>
                                 <span class="font-medium">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
                             </div>
                         </div>
-                        <div class=" mb-3 py-2 text-sm text-black bg-yellow-100 rounded-lg"  role="alert">
+                        <div class=" mb-3 py-2 text-sm text-black bg-yellow-100 rounded-lg cursor-pointer"  role="alert">
                             <div className=' mx-3 pl-2 border-l-4 border-yellow-500 md:pb-5' >
                                 <h1 className='text-lg font-bold'>NITJ Campus2</h1>
                                 <span class="font-medium">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
                             </div>
                         </div>
 
-                        <div class=" mb-3 py-2 text-sm text-black bg-red-100 rounded-lg" role="alert">
+                        <div class=" mb-3 py-2 text-sm text-black bg-red-100 rounded-lg cursor-pointer" role="alert">
                             <div className=' mx-3 pl-2 border-l-4 border-red-500 md:pb-5' >
                                 <h1 className='text-lg font-bold'>NITJ Campus3</h1>
                                 <span class="font-medium">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
                             </div>
                         </div>
 
-                        <div class=" mb-3 py-2 text-sm text-black bg-green-100 rounded-lg " role="alert">
+                        <div class=" mb-3 py-2 text-sm text-black bg-green-100 rounded-lg cursor-pointer" role="alert">
                             <div className=' mx-3 pl-2 border-l-4 border-green-500 md:pb-5' >
                                 <h1 className='text-lg font-bold'>NITJ Campus4</h1>
                                 <span class="font-medium">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
                             </div>
                         </div>
 
-                        <div class=" mb-3 py-2 text-sm text-black bg-blue-100 rounded-lg " role="alert">
+                        <div class=" mb-3 py-2 text-sm text-black bg-blue-100 rounded-lg cursor-pointer" role="alert">
                             <div className=' mx-3 pl-2 border-l-4 border-blue-500 md:pb-5' >
                                 <h1 className='text-lg font-bold'>NITJ Campus5</h1>
                                 <span class="font-medium">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
